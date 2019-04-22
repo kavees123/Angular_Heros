c7658cc59c337f47a0944e01298d15f3254714de
kavees1 <kavees@my.yorku.ca>
2019-04-21 23:45:58 -0400

    Added hero name in the message box

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit,Input } from '@angular/core';
 import {Hero} from '../hero';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-
+import {HEROES} from '../mock-heros';
 import { HeroService }  from '../hero.service';
 @Component({
   selector: 'app-hero-detail',
@@ -19,10 +19,16 @@ export class HeroDetailComponent implements OnInit {
   ngOnInit() {
     this.getHero();
   }
+  myconst = HEROES[12];
   getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id');
+    //console.log("TESTING ID" + id);
+    //console.log("HERO-DETAILS" + this.myconst );
+    this.heroService.sendmessage(id);  
     this.heroService.getHero(id)
-      .subscribe(hero => this.hero = hero);
+      .subscribe(hero => {this.hero = hero;});
+    
+  //  console.log("HERO-DETAILS"+ this.hero.toString());
   }
   goBack(): void {
     this.location.back();
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -10,16 +10,28 @@ import { MessageService } from './message.service';
 export class HeroService {
 
   constructor(private messageService: MessageService) { }
-
+  h = HEROES[10];
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
+   // this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+  getname(id:number){
+  //  return HEROES[id].name;
   }
 
   getHero(id: number): Observable<Hero> {
-    // TODO: send the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
+   
+  
+
     return of(HEROES.find(hero => hero.id === id));
+  }
+  hero: Hero;
+  sendmessage(id:number){
+    this.getHero(id).subscribe(hero => {this.hero = hero; 
+      this.messageService.add(`fetched hero id=${id}`+ "  "+
+      "Name = " + hero.name);});
+
+
   }
 }
